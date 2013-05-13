f957ffaac90ba8844f3cb9113a703b4c8219dc70
Guilhem Lettron <guilhem+github@lettron.fr>
2013-05-13 17:06:56 +0200

    Add uninstall script

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,19 @@
+$package = "zabbix-agent"
+$installDir = Join-Path $env:ProgramFiles "Zabbix Agent"
+
+try
+{
+  # stop helper services if they're running
+  $service = Get-WmiObject -Class Win32_Service -Filter "Name='Zabbix Agent'"
+  $service.StopService()
+  $service.Delete()
+  
+  Remove-Item $installDir
+  
+  Write-ChocolateySuccess $package
+}
+catch
+{
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
