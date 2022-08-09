34680b03154419cfb33640ec8331c16e8f4fda65
Marcin Bojko <marcin@bojko.com.pl>
2022-08-09 11:27:49 +0200

    Version 6.2.1

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # zabbix-agent2
 
-This repository is DEPRECATED and available in READ ONLY mode.
-No more chocolatey packages will be created
+## Overview
+
+Zabbix agent 2 is a new generation of Zabbix agent and may be used in place of Zabbix agent. Zabbix agent 2 has been developed to:
+
+reduce the number of TCP connections
+provide improved concurrency of checks
+be easily extendible with plugins. A plugin should be able to:
+provide trivial checks consisting of only a few simple lines of code
+provide complex checks consisting of long-running scripts and standalone data gathering with periodic sending back of the data
+be a drop-in replacement for Zabbix agent (in that it supports all the previous functionality)
+Agent 2 is written in Go (with some C code of Zabbix agent reused). A configured Go environment with a currently supported Go version is required for building Zabbix agent 2.
+
+Agent 2 does not have built-in daemonization support on Linux; it can be run as a Windows service.
 
 ## Changelog
+
+* 2022-08-09 Version 6.2.1
 
 * 2021-07-27 Version 5.2.7
 
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,15 +1,46 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName        = 'zabbix-agent2'
-$version            = '5.2.7'
-$url64              = "https://cdn.zabbix.com/zabbix/binaries/stable/5.2/$version/zabbix_agent2-$version-windows-amd64-openssl.msi"
-$checksum64         = "6c4183bcdae364c4df2b0fa72f70430acbbcef5ae11b059c91f68a63a565a7f1"
+$version            = '6.2.1'
+$url64              = "https://cdn.zabbix.com/zabbix/binaries/stable/6.2/$version/zabbix_agent2-$version-windows-amd64-openssl.msi"
+$checksum64         = "cf3babd1718591dd8336d5b4b2282455833b999a407891863d40f8e6df250640"
 $installFolder      = "$Env:ProgramFiles\zabbix-agent2"
+$silentArgs         = "/qn /norestart /l*v zabbix-log.txt"
+#$silentArgs         = "/qn /norestart"
+# Parameters
+$pp = Get-PackageParameters
+if ($pp.LOGTYPE) { $SilentArgs += " LOGTYPE=`"$($pp.LOGTYPE)`"" }
+if ($pp.LOGFILE) { $SilentArgs += " LOGFILE=`"$($pp.LOGFILE)`"" }
+if ($pp.SERVER) { $SilentArgs += " SERVER=`"$($pp.SERVER)`"" } else { $SilentArgs += " SERVER=`"127.0.0.1`"" }
+if ($pp.LISTENPORT) { $SilentArgs += " LISTENPORT=`"$($pp.LISTENPORT)`"" }
+if ($pp.SERVERACTIVE) { $SilentArgs += " SERVERACTIVE=`"$($pp.SERVERACTIVE)`"" }
+if ($pp.HOSTNAME) { $SilentArgs += " HOSTNAME=`"$($pp.HOSTNAME)`"" }
+if ($pp.TIMEOUT) { $SilentArgs += " TIMEOUT=`"$($pp.TIMEOUT)`"" }
+if ($pp.TLSCONNECT) { $SilentArgs += " TLSCONNECT=`"$($pp.TLSCONNECT)`"" }
+if ($pp.TLSACCEPT) { $SilentArgs += " TLSACCEPT=`"$($pp.TLSACCEPT)`"" }
+if ($pp.TLSPSKIDENTITY) { $SilentArgs += " TLSPSKIDENTITY=`"$($pp.TLSPSKIDENTITY)`"" }
+if ($pp.TLSPSKFILE) { $SilentArgs += " TLSPSKFILE=`"$($pp.TLSPSKFILE)`"" }
+if ($pp.TLSPSKVALUE) { $SilentArgs += " TLSPSKVALUE=`"$($pp.TLSPSKVALUE)`"" }
+if ($pp.TLSCAFILE) { $SilentArgs += " TLSCAFILE=`"$($pp.TLSCAFILE)`"" }
+if ($pp.TLSCRLFILE) { $SilentArgs += " TLSCRLFILE=`"$($pp.TLSCRLFILE)`"" }
+if ($pp.TLSSERVERCERTISSUER) { $SilentArgs += " TLSSERVERCERTISSUER=`"$($pp.TLSSERVERCERTISSUER)`"" }
+if ($pp.TLSSERVERCERTSUBJECT) { $SilentArgs += " TLSSERVERCERTSUBJECT=`"$($pp.TLSSERVERCERTSUBJECT)`"" }
+if ($pp.TLSCERTFILE) { $SilentArgs += "  TLSCERTFILE=`"$($pp.TLSCERTFILE)`"" }
+if ($pp.TLSKEYFILE) { $SilentArgs += " TLSKEYFILE=`"$($pp.TLSKEYFILE)`"" }
+if ($pp.LISTENIP) { $SilentArgs += " LISTENIP=`"$($pp.LISTENIP)`"" }
+if ($pp.HOSTINTERFACE) { $SilentArgs += " HOSTINTERFACE=`"$($pp.HOSTINTERFACE)`"" }
+if ($pp.HOSTMETADATA) { $SilentArgs += " HOSTMETADATA=`"$($pp.HOSTMETADATA)`"" }
+if ($pp.HOSTMETADATAITEM) { $SilentArgs += " HOSTMETADATAITEM=`"$($pp.HOSTMETADATAITEM)`"" }
+if ($pp.INSTALLFOLDER) { $SilentArgs += " INSTALLFOLDER=`"$($pp.INSTALLFOLDER)`"" } else { $SilentArgs += " INSTALLFOLDER=`"$installFolder`"" }
+if ($pp.ENABLEPATH) { $SilentArgs += " ENABLEPATH=`"$($pp.ENABLEPATH)`"" } else { $SilentArgs += " ENABLEPATH=1" }
+if ($pp.SKIP) { $SilentArgs += " SKIP=`"$($pp.SKIP)`"" } else { $SilentArgs += " SKIP=fw" }
+if ($pp.INCLUDE) { $SilentArgs += " INCLUDE=`"$($pp.INCLUDE)`"" }
+if ($pp.ALLOWDENYKEY) { $SilentArgs += " ALLOWDENYKEY=`"$($pp.ALLOWDENYKEY)`"" }
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'msi'
-  silentArgs    = "/qn /norestart /l*v zabbix-log.txt ENABLEPATH=1 SKIP=fw SERVER=127.0.0.1 INSTALLFOLDER=`"$installFolder`""
+  silentArgs    = $SilentArgs
   validExitCodes= @(0, 3010, 1641)
   url64bit      = $url64
   checksumType64= 'sha256'
